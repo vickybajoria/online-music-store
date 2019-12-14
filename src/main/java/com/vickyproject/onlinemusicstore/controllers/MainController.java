3368040550564666f3fe74e320513c971fa2bce5
Vicky Bajoria <vickybajoria@gmail.com>
2019-12-14 17:07:26 +0530

    provided online-music-store REST API

--- src/main/java/com/vickyproject/onlinemusicstore/controllers/MainController.java
+++ src/main/java/com/vickyproject/onlinemusicstore/controllers/MainController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class MainController {
 	
-	@RequestMapping("/")
+	@RequestMapping("online-music-store")
 	public String homePage()
 	{
 		return "home";
