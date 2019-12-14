6cdb1b665407e2571f48cb81ad9cd74b0d696597
Vicky Bajoria <vickybajoria@gmail.com>
2019-12-14 16:18:59 +0530

    Added Spring boot starter project - folder structure

---
+++ src/main/java/com/vickyproject/onlinemusicstore/OnlineMusicStoreApplication.java
@@ -0,0 +1,13 @@
+package com.vickyproject.onlinemusicstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMusicStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMusicStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vickyproject/onlinemusicstore/OnlineMusicStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vickyproject.onlinemusicstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMusicStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
