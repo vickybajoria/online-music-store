384575c9c1b676e7a85f7808097557f8924c35e5
Vicky Bajoria <vickybajoria@gmail.com>
2019-12-15 20:00:23 +0530

    Added productList API. and added productList page

--- src/main/java/com/vickyproject/onlinemusicstore/controllers/MainController.java
+++ src/main/java/com/vickyproject/onlinemusicstore/controllers/MainController.java
@@ -1,16 +1,34 @@
 package com.vickyproject.onlinemusicstore.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.vickyproject.onlinemusicstore.dao.ProductDao;
+import com.vickyproject.onlinemusicstore.entity.Product;
+
 @Controller
-@RequestMapping("/")
+@RequestMapping("/online-music-store")
 public class MainController {
 	
-	@RequestMapping("online-music-store")
+	ProductDao dao = new ProductDao();
+	
+	@RequestMapping("/")
 	public String homePage()
 	{
 		return "home";
+	}
+	
+	@RequestMapping("/productList")
+	public String getProduct(Model theModel)
+	{
+		List<Product> prodList = dao.getProductList();
+		
+		theModel.addAttribute("prod1", prodList.get(0));
+		
+		return "productList";
 	}
 
 }
